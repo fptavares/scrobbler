1d69b0962800d455116392221d420f49af1471e2
Filipe Tavares <fptavares@gmail.com>
2020-02-16 22:04:48 +0100

    Fixed and centered the empty state message

--- lib/components/emtpy.dart
+++ lib/components/emtpy.dart
@@ -16,36 +16,44 @@ class EmptyState extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(
-            top: 30.0,
-            bottom: 15.0,
-            left: 15.0,
-            right: 15.0,
-          ),
-          child: Image(image: AssetImage(imagePath)),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            headline,
-            style: theme.textTheme.display1.copyWith(
-              color: theme.accentColor,
-              fontWeight: FontWeight.w500,
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 30.0,
+                bottom: 15.0,
+                left: 15.0,
+                right: 15.0,
+              ),
+              child: Image(image: AssetImage(imagePath)),
             ),
           ),
-        ),
-        Text(
-          subhead,
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.subhead.copyWith(
-            color: Colors.black54,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              headline,
+              style: theme.textTheme.display1.copyWith(
+                color: theme.accentColor,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ),
-        ),
-      ],
+          Text(
+            subhead,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.subhead.copyWith(
+              color: Colors.black54,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
