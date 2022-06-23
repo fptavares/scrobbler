752746bc00d7e4722257a3b815af6f230e3f8220
Filipe Tavares <fptavares@gmail.com>
2022-06-23 20:55:34 +0300

    Changed debug logging level to INFO

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -59,7 +59,7 @@ Future<void> main() async {
         }
       });
     } else {
-      Logger.root.level = Level.ALL; // defaults to Level.INFO
+      Logger.root.level = Level.INFO; // defaults to Level.INFO
       Logger.root.onRecord.listen((record) {
         print('[${record.level.name}] ${record.loggerName}: ${record.message}'); // ignore: avoid_print
         if (record.level > Level.INFO) {
