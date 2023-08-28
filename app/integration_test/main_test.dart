2be67d02abd4eab53a8c506a047e94991754be51
fptavares <fptavares@gmail.com>
2023-08-28 11:45:33 +0300

    Integration test using real Discogs backend

--- app/integration_test/main_test.dart
+++ app/integration_test/main_test.dart
@@ -1,22 +1,12 @@
-import 'dart:io';
-import 'dart:ui';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
-import 'package:flutter_cache_manager/src/cache_store.dart';
-import 'package:flutter_cache_manager/src/web/web_helper.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
 import 'package:scrobbler/components/album.dart';
 import 'package:scrobbler/components/playlist.dart';
 import 'package:scrobbler/firebase_options.dart';
 import 'package:scrobbler/main.dart';
-import 'package:scrobbler/model/discogs.dart';
 import 'package:scrobbler/model/settings.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import 'fake_collection_test_data.dart';
 
 void main() {
   late final SharedPreferences prefs;
@@ -25,65 +15,20 @@ void main() {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-    WidgetsApp.debugAllowBannerOverride = false; // remove debug banner
 
     SharedPreferences.setMockInitialValues({
-      Settings.discogsUsernameKey: 'someuser',
+      Settings.discogsUsernameKey: 'scrobbler_app',
       Settings.skippedKey: false,
       Settings.lastfmUsernameKey: 'someuser',
       Settings.lastfmSessionKeyKey: '',
     });
     prefs = await SharedPreferences.getInstance();
-
-    final config = Config('testCache');
-    Collection.cache = CacheManager.custom(config,
-        webHelper: WebHelper(CacheStore(config), HttpFileService(httpClient: StaticCollectionHttpClient())));
-    await Collection.cache.emptyCache();
-
-    if (Platform.isIOS) {
-      // Even though the images used are not eligible for copyright,
-      // and thus considered public domain,
-      // the iOS app store doesn't approve their use in app screenshots.
-      // So on iOS this will hide them and overlay the explanation text.
-      AlbumImage.imageBuilder = (image) => AspectRatio(
-            aspectRatio: 1,
-            child: Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                Image(image: image),
-                Center(
-                  child: ClipRect(
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(
-                        sigmaX: 10.0,
-                        sigmaY: 10.0,
-                      ),
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 150.0,
-                        height: 150.0,
-                        padding: const EdgeInsets.all(5.0),
-                        child: const FittedBox(
-                          fit: BoxFit.fitWidth,
-                          child: Text(
-                            'Image hidden\ndue to\ncopyright',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-    }
   });
 
   testWidgets('Scrobbler app main use cases work', (tester) async {
-    final album1 = find.byKey(const ValueKey<int>(454789538));
+    final album1 = find.byTooltip('Graceland by Paul Simon');
     final album1Text = find.text('Paul Simon');
-    final album2 = find.byKey(const ValueKey<int>(454789997));
+    final album2 = find.byTooltip('Pure Heroine by Lorde');
 
     // load the App
     await tester.pumpWidget(ScrobblerApp(prefs, 'ScrobblerIntegrationTest'));
@@ -180,22 +125,4 @@ void main() {
 
     // take screenshot of time selection
   });
-}
-
-class StaticCollectionHttpClient extends BaseClient {
-  StaticCollectionHttpClient();
-
-  final _client = Client();
-
-  @override
-  Future<StreamedResponse> send(BaseRequest request) async {
-    if (request.url.path.contains('/collection/folders/0/releases')) {
-      return StreamedResponse(
-        Stream<List<int>>.fromIterable(<List<int>>[fakeCollectionData.codeUnits]),
-        200,
-      );
-    } else {
-      return await _client.send(request);
-    }
-  }
 }
